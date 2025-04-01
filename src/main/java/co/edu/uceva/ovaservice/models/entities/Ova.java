b433d7d0c4755507881ee23dfab737be2f1374bf
Camilo <juan.suarez04@uceva.edu.co>
2025-03-31 20:47:29 -0500

    Se corrigio el error de la paginacion y se agrego un nueva entidad llamada id_curso

--- src/main/java/co/edu/uceva/ovaservice/models/entities/Ova.java
+++ src/main/java/co/edu/uceva/ovaservice/models/entities/Ova.java
@@ -5,22 +5,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Ova {
 @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nombre;
     private String descripcion;
+    private long idCurso;
 
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into ova (nombre, descripcion,) values ('Ova 1', 'Descripcion 1');
-insert into ova (nombre, descripcion,) values ('Ova 2', 'Descripcion 2');
-insert into ova (nombre, descripcion,) values ('Ova 3', 'Descripcion 3');
-insert into ova (nombre, descripcion,) values ('Ova 4', 'Descripcion 4');
-insert into ova (nombre, descripcion,) values ('Ova 5', 'Descripcion 5');
+insert into ova (nombre, descripcion,id_curso) values ('Ova 1', 'Descripcion 1',1);
+insert into ova (nombre, descripcion,id_curso) values ('Ova 2', 'Descripcion 2',2);
+insert into ova (nombre, descripcion,id_curso) values ('Ova 3', 'Descripcion 3',3);
+insert into ova (nombre, descripcion,id_curso) values ('Ova 4', 'Descripcion 4',4);
+insert into ova (nombre, descripcion,id_curso) values ('Ova 5', 'Descripcion 5',5);
