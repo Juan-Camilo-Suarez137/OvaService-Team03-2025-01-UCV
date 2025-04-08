628cad5880249f73b0fb70c248b8574a9e264352
Camilo <juan.suarez04@uceva.edu.co>
2025-04-07 22:13:01 -0500

    se organizo el endpoint

--- src/main/java/co/edu/uceva/ovaservice/delivery/exception/GlobalExceptionHandler.java
+++ src/main/java/co/edu/uceva/ovaservice/delivery/exception/GlobalExceptionHandler.java
@@ -5,11 +5,12 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final String ERROR = "error";
     private static final String MENSAJE = "mensaje";
